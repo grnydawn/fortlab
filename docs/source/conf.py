561ced8ef04010df8fcbfe45d8262878f86ff686
Youngsung Kim <kimy@ornl.gov>
2022-10-04 15:04:49 -0400

    updated doc version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Youngsung Kim'
 author = 'Youngsung Kim'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.15'
+release = '1.1.0'
 
 # -- General configuration ---------------------------------------------------
 
